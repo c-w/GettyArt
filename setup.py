46b723ad4b48e29225cf8fcc44fa90bc9cfc3e21
Clemens Wolff <clemens.wolff+git@gmail.com>
2014-10-25 14:34:46 +0100

    Enable distutils for package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='Getty',
+    version='0.0.1',
+    author='Clemens Wolff',
+    author_email='clemens.wolff+pypi@gmail.com',
+    packages=['getty'],
+    url='https://github.com/c-w/Getty',
+    download_url='http://pypi.python.org/pypi/Getty',
+    license='LICENSE.txt',
+    description='Scraper for art available from getty.edu',
+    long_description=open('README.rst').read(),
+)
